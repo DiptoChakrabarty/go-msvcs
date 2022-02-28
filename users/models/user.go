0a038c5f5e5331180ce2486e5e621268ce760ec4
DiptoChakrabarty <diptochuck123@gmail.com>
2022-02-28 18:04:57 +0530

    gorm model

--- users/models/user.go
+++ users/models/user.go
@@ -7,11 +7,11 @@ import (
 )
 
 type User struct {
-	Id        int64  `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Created   string `json:"created"`
+	Id        int64  `gorm:"primary_key;auto_increment" json:"id"`
+	FirstName string `json:"first_name" binding:"required" gorm:"type:varchar(10)"`
+	LastName  string `json:"last_name" binding:"required" gorm:"type:varchar(10)"`
+	Email     string `json:"email" binding:"required" gorm:"type:varchar(10)"`
+	Created   string `json:"created" gorm:"default:CURRENT_TIMESTAMP"`
 }
 
 func (usr User) Validate() *resterrors.RestErr {
