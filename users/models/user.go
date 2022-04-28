58777050374e7529c44b0b8bdf4fb5c5dd9d874b
DiptoChakrabarty <diptochuck123@gmail.com>
2022-04-28 16:21:30 +0530

    password checking

--- users/models/user.go
+++ users/models/user.go
@@ -8,16 +8,22 @@ import (
 
 type User struct {
 	ID        uint64 `gorm:"primaryKey;auto_increment" json:"id"`
-	FirstName string `json:"first" binding:"required" gorm:"type:varchar(10)"`
-	LastName  string `json:"last" binding:"required" gorm:"type:varchar(10)"`
-	Email     string `json:"email" binding:"required" gorm:"type:varchar(10)"`
+	FirstName string `json:"first" binding:"required" gorm:"type:varchar(20)"`
+	LastName  string `json:"last" binding:"required" gorm:"type:varchar(20)"`
+	Email     string `json:"email" binding:"required" gorm:"type:varchar(20)"`
 	Created   string `json:"-" gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	Password  string `json:"password" binding:"required" gorm:"type:varchar(20)"`
 }
 
 func (usr User) Validate() *resterrors.RestErr {
 	usr.Email = strings.TrimSpace(strings.ToLower(usr.Email))
 	if usr.Email == "" {
 		return resterrors.BadRequestError("invalid email address")
+	}
+
+	usr.Password = strings.TrimSpace(usr.Password)
+	if usr.Password == "" {
+		return resterrors.BadRequestError("empty password provided")
 	}
 	return nil
 }
