a74acf31f059e71bbcafc5c49fc93bae8f638960
DiptoChakrabarty <diptochuck123@gmail.com>
2022-05-12 10:18:05 +0530

    beggin oauth part

---
+++ auth/access-token.go
@@ -0,0 +1,20 @@
+package auth
+
+import "time"
+
+const (
+	expirationTime = 24
+)
+
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	UserId      int    `json:"user_id"`
+	ClientId    int    `json:"user_id"`
+	Expires     int64  `json:"expires"`
+}
+
+func GenerateAccessToken() AccessToken {
+	return AccessToken{
+		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
+	}
+}
