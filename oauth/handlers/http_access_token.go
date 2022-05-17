ec888d084a597714d83cf10aad046aec78a5e1f2
DiptoChakrabarty <diptochuck123@gmail.com>
2022-05-17 19:44:22 +0530

    get id and invoke service

--- oauth/handlers/http_access_token.go
+++ oauth/handlers/http_access_token.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/DiptoChakrabarty/go-mvcs/oauth/access_token_service"
 	"github.com/gin-gonic/gin"
@@ -21,6 +22,12 @@ func NewHandler(svc access_token_service.AccessTokenService) AccessTokenHandler 
 	}
 }
 
-func (h accessTokenHandler) GetById(ctx *gin.Context) {
-	ctx.JSON(http.StatusNotImplemented, "Implement Me")
+func (hdr accessTokenHandler) GetById(ctx *gin.Context) {
+	accessTokenID := strings.TrimSpace(ctx.Param("access_token_id"))
+	accessToken, err := hdr.service.GetById(accessTokenID)
+	if err != nil {
+		ctx.JSON(err.Status, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, accessToken)
 }
