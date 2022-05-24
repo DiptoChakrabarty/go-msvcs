b99a2f0949f9b3bec96252297bfad7f912a22222
DiptoChakrabarty <diptochuck123@gmail.com>
2022-05-24 11:46:13 +0530

    create keyspace

--- oauth/client/cassandra/client.go
+++ oauth/client/cassandra/client.go
@@ -16,4 +16,11 @@ func Connect() {
 	}
 	defer session.Close()
 
+	// creating keyspace
+	err = session.Query("CREATE KEYSPACE IF NOT EXISTS oauth WITH REPLICATION= {'class': 'NetworkTopologyStrategy'};").Exec()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 }
