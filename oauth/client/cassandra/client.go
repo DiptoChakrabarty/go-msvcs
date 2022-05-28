a40c561d4b51f9701e30ee306e5bf68437063681
DiptoChakrabarty <diptochuck123@gmail.com>
2022-05-28 19:57:59 +0530

    get session from db

--- oauth/client/cassandra/client.go
+++ oauth/client/cassandra/client.go
@@ -1,11 +1,16 @@
 package cassandra
 
 import (
+	"errors"
 	"log"
 	"os"
 
 	"github.com/gocql/gocql"
 	"github.com/joho/godotenv"
+)
+
+var (
+	cluster *gocql.ClusterConfig
 )
 
 func getenvValue(key string) string {
@@ -19,10 +24,15 @@ func getenvValue(key string) string {
 func init() {
 	cluster := gocql.NewCluster("127.0.0.1")
 	cluster.Authenticator = gocql.PasswordAuthenticator{Username: getenvValue("username"), Password: getenvValue("password")}
+	cluster.Consistency = gocql.Quorum
+
+}
+
+func GetDBSession() (*gocql.Session, error) {
 	session, err := cluster.CreateSession()
 	if err != nil {
 		log.Println(err)
-		return
+		return nil, errors.New("Unable to create session")
 	}
 	defer session.Close()
 
@@ -30,14 +40,15 @@ func init() {
 	err = session.Query("CREATE KEYSPACE IF NOT EXISTS oauth WITH REPLICATION= {'class': 'NetworkTopologyStrategy'};").Exec()
 	if err != nil {
 		log.Println(err)
-		return
+		return nil, errors.New("Unable to get keyspace")
 	}
+	cluster.Keyspace = "oauth"
 
 	// creating table
 	err = session.Query("CREATE TABLE IF NOT EXISTS oauth.users (name text, access_token text, id int), PRIMARY KEY (id));").Exec()
 	if err != nil {
 		log.Println(err)
-		return
+		return nil, errors.New("Unable to get table")
 	}
-
+	return session, nil
 }
