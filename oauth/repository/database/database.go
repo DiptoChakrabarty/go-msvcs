b853cb4cb9cbfc6386aa9b5d30f6b91fa52ea757
DiptoChakrabarty <diptochuck123@gmail.com>
2022-06-07 18:26:40 +0530

    update access token expiry time

--- oauth/repository/database/database.go
+++ oauth/repository/database/database.go
@@ -8,13 +8,15 @@ import (
 )
 
 const (
-	queryGetAccessToken = "SELECT access_token, user_id, client_id, expires FROM access_token WHERE access_token=?"
-	queryCreateToken    = "INSERT INTO access_token(access_token, user_id, client_id expires) VALUES (?,?,?,?);"
+	queryGetAccessToken   = "SELECT access_token, user_id, client_id, expires FROM access_token WHERE access_token=?"
+	queryCreateToken      = "INSERT INTO access_token(access_token, user_id, client_id expires) VALUES (?,?,?,?);"
+	queryUpdateExpiryTime = "UPDATE access_token SET expires=? WHERE access_token=?;"
 )
 
 type DBRepository interface {
 	GetById(string) (*access_token.AccessToken, resterrors.RestErr)
 	Create(access_token.AccessToken) resterrors.RestErr
+	UpdateExpiryTime(access_token.AccessToken) resterrors.RestErr
 }
 
 type dbrepository struct{}
@@ -52,14 +54,30 @@ func (db dbrepository) Create(at access_token.AccessToken) resterrors.RestErr {
 	}
 	defer session.Close()
 
-	if err := session.Query(queryCreateToken, 
-				at.AccessToken,
-				at.UserId,
-				at.ClientId,
-				at.Expires).Exec(); err != nil {
-					logger.Error("Unable to create the access token", err)
-					return re
-				}
+	if err := session.Query(queryCreateToken,
+		at.AccessToken,
+		at.UserId,
+		at.ClientId,
+		at.Expires).Exec(); err != nil {
+		logger.Error("Unable to create the access token", err)
+		return resterrors.InternalServerError("Unable to create access token")
+	}
+	return nil
+}
 
+func (db dbrepository) UpdateExpiryTime(at access_token.AccessToken) resterrors.RestErr {
+	session, err := cassandra.GetDBSession()
+	if err != nil {
+		logger.Error("Error while creating DB Session", err)
+		return resterrors.InternalServerError("Errror creating a DB Session", err)
+	}
+	defer session.Close()
+
+	if err := session.Query(queryUpdateExpiryTime,
+		at.Expires,
+		at.AccessToken).Exec(); err != nil {
+		logger.Error("Unable to update access token expiry time", err)
+		return resterrors.InternalServerError("Unable to update access token expiry time")
+	}
 	return nil
 }
